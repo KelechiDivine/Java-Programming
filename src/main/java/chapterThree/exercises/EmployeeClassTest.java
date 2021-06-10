12180160f12eae3c4ec8ad52ff3f7b195ffd2f62
Kelechi Divine <74909446+KelechiDivine@users.noreply.github.com>
2021-06-10 12:30:53 +0100

    invoice completed

--- src/main/java/chapterThree/exercises/EmployeeClassTest.java
+++ src/main/java/chapterThree/exercises/EmployeeClassTest.java
@@ -4,8 +4,8 @@ public class EmployeeClassTest {
 	
 	public static void main(String[] args){
 	
-	EmployeeClass kelechiEmployeeClass = new EmployeeClass("Kelechi", "Okoroafor", 4000000);
-	EmployeeClass estherEmployeeClass = new EmployeeClass("Esther", "Omolola", 200000);
+	EmployeeClass kelechiEmployeeClass = new EmployeeClass("Kelechi", "Okoroafor", 356724000);
+	EmployeeClass estherEmployeeClass = new EmployeeClass("Esther", "Omolola", 5409760);
 	
 	System.out.print("\nEmployer\t\tEmployers Name\t\t\tMonthly Salary\n");
 	System.out.printf("S/n:\t\t%10s %10s\t\t%5s%.2f\n", kelechiEmployeeClass.getFirstName(),
@@ -17,14 +17,26 @@ public class EmployeeClassTest {
 	
 //	Set raise by 10%
 		
-		if (kelechiEmployeeClass.getMonthlySalary() < 0)
+//		If the monthly salary is not positive, do not set its value.
+		
+		if (kelechiEmployeeClass.getMonthlySalary() > 0)
 			kelechiEmployeeClass.setMonthlySalary(0.1 * kelechiEmployeeClass.getMonthlySalary()
 					+ kelechiEmployeeClass.getMonthlySalary());
 		
-		if (estherEmployeeClass.getMonthlySalary() < 0)
+		if (estherEmployeeClass.getMonthlySalary() > 0)
 			estherEmployeeClass.setMonthlySalary(0.1 * estherEmployeeClass.getMonthlySalary()
 					+ estherEmployeeClass.getMonthlySalary());
-	
+		
+		
+		System.out.print("\n10 Percent Salary Raised!!!\n");
+		
+		System.out.print("\nEmployer\t\tEmployers Name\t\t\tMonthly Salary\n");
+		
+		System.out.printf("S/n:\t\t%10s %10s\t\t%5s%.2f\n", kelechiEmployeeClass.getFirstName(),
+				kelechiEmployeeClass.getLastName(), "$ ", kelechiEmployeeClass.getMonthlySalary());
+		
+		System.out.printf("S/n:\t\t%10s %10s\t\t%5s%.2f\n", estherEmployeeClass.getFirstName(),
+				estherEmployeeClass.getLastName(), "$ ", estherEmployeeClass.getMonthlySalary());
 	
 	
 	}
