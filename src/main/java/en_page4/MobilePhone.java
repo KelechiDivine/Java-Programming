05bfcab3aa90198dadb074683978551448849743
Kelechi Divine <74909446+KelechiDivine@users.noreply.github.com>
2021-09-06 11:47:09 +0100

    ...

--- src/main/java/en_page4/MobilePhone.java
+++ src/main/java/en_page4/MobilePhone.java
@@ -1,6 +1,53 @@
 package en_page4;
 
 public class MobilePhone {
-
-
+	
+	public void search(){
+		System.out.print("1: Search");
+	}
+	
+	public void service_number(){
+		System.out.print("2 Service Number");
+	}
+	
+	public void add_name(){
+		System.out.print("3: Add name");
+	}
+	
+	public void erase() {
+		System.out.print("4: Erase");
+	}
+	
+	public void edit() {
+		System.out.print("5: Edit");
+	}
+	
+	public void assign_tone() {
+		System.out.print("6: Assign tone");
+	}
+	
+	public void send_b_card() {
+		System.out.print("7: Send b's card");
+	}
+	
+	public void option() {
+		System.out.print("8: Options");
+		System.out.print(navigate_options(8));
+	}
+	
+	public boolean navigate_options(int is_navigate) {
+		if (is_navigate == 8) {
+			System.out.print("\t-> Type Of view");
+			System.out.print("\t-> Memory status");
+		}
+		return false;
+	}
+	
+	public void speed_dials() {
+		System.out.print("9: Speed Dials");
+	}
+	
+	public void voice_tags() {
+		System.out.print("10: Voice Tags");
+	}
 }
--- src/test/java/en_page4/MobilePhoneTest.java
+++ src/test/java/en_page4/MobilePhoneTest.java
@@ -2,6 +2,20 @@ package en_page4;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class MobilePhoneTest {
+public class MobilePhoneTest {
 
+	pr
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
