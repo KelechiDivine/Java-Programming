287954d64651f6427bbc59eb8ae954c5f49e6dc0
Kelechi Divine <74909446+KelechiDivine@users.noreply.github.com>
2021-09-07 14:57:16 +0100

    created the employee class

--- src/main/java/workingWithAyomide/DateClass.java
+++ src/main/java/workingWithAyomide/DateClass.java
@@ -1,20 +1,36 @@
-//package workingWithAyomide;
-//
-//public class DateClass {
-//
-//	private int month;
-//	private int day;
-//	private int year;
-//
-//	private static final int[] daysPerMonth = {
-//			0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31
-//	};
-//
-//	public DateClass(int month, int day, int year){
-//		if (month <= 0 || month > 12)
-//			throw new IllegalStateException("Month (" + month + ") " +
-//					"must be 1-12");
-//
-//		if(day <= 0 || (day > daysPerMonth[month]))
-//	}
-//}
+package workingWithAyomide;
+
+public class DateClass {
+
+	private final int month;
+	private final int day;
+	private final int year;
+
+	private static final int[] daysPerMonth = {
+			0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31
+	};
+
+	public DateClass(int month, int day, int year){
+		if (month <= 0 || month > 12) {
+			throw new IllegalStateException("Month (" + month + ") " +
+					"must be 1-12");
+		}
+		if(day <= 0 || (day > daysPerMonth[month]&& !(month == 2
+		&& day == 29))){
+			throw new IllegalArgumentException("day (" + day +
+					") out-of-range for the specified month and year");
+			
+		}
+		
+		this.month = month;
+		this.day = day;
+		this.year = year;
+		
+		System.out.printf("Date object constructor for date" +
+				"%s%n", this);
+	}
+	
+	public String toString(){
+		return String.format("%d/%d/%d", month, day, year);
+	}
+}
---
+++ src/main/java/workingWithAyomide/Employee.java
@@ -0,0 +1,26 @@
+package workingWithAyomide;
+
+import java.util.Date;
+
+public class Employee {
+	
+	private String firstName;
+	private String lastName;
+	private Date birthDate;
+	private Date hireDate;
+	
+	
+	public Employee(String firstName, String lastName, Date
+					birthDate, Date hireDate){
+		
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.birthDate = birthDate;
+		this.hireDate = hireDate;
+	}
+	
+	public String toString(){
+		return String.format("%s, %s, Hired: %s Birthday: %s",
+				lastName, firstName, hireDate, birthDate);
+	}
+}
