22b35a4ef9f50ba0629034c3043c2c13b008f970
Kelechi Divine <74909446+KelechiDivine@users.noreply.github.com>
2021-09-07 19:07:24 +0100

    finished creating enum class and enum test class

--- src/main/java/workingWithAyomide/BookTest.java
+++ src/main/java/workingWithAyomide/BookTest.java
@@ -1,4 +1,21 @@
 package workingWithAyomide;
 
-public enum BookTest {
+import java.util.EnumSet;
+
+public class BookTest {
+	
+	public static void main(String[] args) {
+		System.out.println("All books");
+		
+		for(Book book : Book.values()){
+			System.out.printf("%-10s%-45s%s%n", book,
+					book.getTitle(), book.getCopyrightYear());
+			System.out.printf("%nDisplay a range of enum constants:%n");
+		}
+		
+		for (Book book : EnumSet.range(Book.JHTP, Book.CPPHTP)){
+			System.out.printf("%-10s%-45s%s%n", book,
+					book.getTitle(), book.getCopyrightYear());
+		}
+	}
 }
