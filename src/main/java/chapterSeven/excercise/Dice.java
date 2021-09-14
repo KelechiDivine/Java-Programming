c2103989167c9fb894155f132a82c8897c7b0264
Kelechi Divine <74909446+KelechiDivine@users.noreply.github.com>
2021-09-14 11:56:40 +0100

    created a method the rolls only one dice

--- src/main/java/chapterSeven/excercise/Dice.java
+++ src/main/java/chapterSeven/excercise/Dice.java
@@ -1,6 +1,29 @@
 package chapterSeven.excercise;
 
+import Registery.Exceptions.InputOutOfBoundException;
+
+import java.security.SecureRandom;
+
 public class Dice {
-
-
+	
+	
+	SecureRandom random = new SecureRandom();
+	int[] firstDice = new int[7];
+	
+	public SecureRandom getRandom() {
+		random.nextInt(firstDice[7]);
+		return random;
+	}
+	
+	public void getFirstDice(int rollDice) throws InputOutOfBoundException {
+		if (rollDice > firstDice[7]) {
+			throw new ArrayIndexOutOfBoundsException();
+		} else {
+			random.nextInt(rollDice);
+		}
+	}
+	
+	public SecureRandom returnRolledDiceResult(){
+		return random;
+	}
 }
--- src/test/java/chapterSeven/excercise/DiceTest.java
+++ src/test/java/chapterSeven/excercise/DiceTest.java
@@ -1,7 +1,23 @@
 package chapterSeven.excercise;
 
-import static org.junit.jupiter.api.Assertions.*;
 
-class DiceTest {
+import Registery.Exceptions.InputOutOfBoundException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
+public class DiceTest {
+	
+	private Dice dice;
+	
+	@BeforeEach
+	public void setDice(){
+		dice = new Dice();
+	}
+	
+	@Test
+	public void rollSingleDice() throws InputOutOfBoundException {
+		dice.getFirstDice(2);
+		Assertions.assertEquals(2, dice.returnRolledDiceResult());
+	}
 }
