f2eff4f1a720f2c4f7f5acdebd324d9970ce7de9
Kelechi Divine <74909446+KelechiDivine@users.noreply.github.com>
2021-09-22 00:17:50 +0100

    override

--- src/main/java/chapterNine/CommissionEmployeeTest.java
+++ src/main/java/chapterNine/CommissionEmployeeTest.java
@@ -1,4 +1,33 @@
 package chapterNine;
 
 public class CommissionEmployeeTest {
+	
+	public static void main(String[] args) {
+		
+		CommissionEmployee commissionEmployee = new CommissionEmployee(
+				"sue","Jones", "2222222", 1000,
+				.06
+		);
+		
+		System.out.println(
+				"Employee information obtained by get methods: "
+		);
+		
+		System.out.printf("%n%s %s%n" +
+				" first name is ", commissionEmployee.getFirstName());
+		
+		System.out.printf("%n%s %s%n" +
+				" last name is ", commissionEmployee.getFirstName());
+		
+		System.out.printf("%n%s %s%n" +
+				" social security number is ", commissionEmployee.getSocialSecurityNumber());
+		
+		System.out.printf("%n%s %s%n" +
+				" gross is ", commissionEmployee.getGrossSales());
+		
+		System.out.printf("%n%s %s%n" +
+				"commission rate is ", commissionEmployee.getCommissionRate());
+		
+//		commissionEmployee.set
+	}
 }
