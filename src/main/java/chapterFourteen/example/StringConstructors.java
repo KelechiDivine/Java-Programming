d6f6afb2c2ef8f3951d4d006d15fc2c68819ee1d
Kelechi Divine <74909446+KelechiDivine@users.noreply.github.com>
2021-09-22 16:03:16 +0100

    string constructoe

---
+++ src/main/java/chapterFourteen/example/StringConstructors.java
@@ -0,0 +1,20 @@
+package chapterFourteen.example;
+
+public class StringConstructors {
+	
+	public static void main(String[] args) {
+		char[] charArray = {
+				'b', 'i', 'r', 't', 'h', ' ' ,'d', 'a', 'y'
+		};
+		
+		String s = new String("Hello");
+		
+		String s1 = "";
+		String s2 = new String(s);
+		String s3 = new String(charArray);
+		String s4 = new String(charArray);
+		
+		System.out.printf("s1 = %s%ns2 = %s%ns3 = %s%ns4 = %s%n",
+				s1, s2, s3, s4);
+	}
+}
