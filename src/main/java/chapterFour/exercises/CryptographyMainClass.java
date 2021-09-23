b75803e9c18a51b6ed143ebd312e699cd867a517
Kelechi Divine <74909446+KelechiDivine@users.noreply.github.com>
2021-09-23 01:00:33 +0100

    Done with cryptography

--- src/main/java/chapterFour/exercises/CryptographyMainClass.java
+++ src/main/java/chapterFour/exercises/CryptographyMainClass.java
@@ -4,10 +4,27 @@ import java.util.Scanner;
 
 public class CryptographyMainClass {
 	
-	Scanner scanner = new Scanner(System.in);
-	
-	Cryptography cryptography = new Cryptography();
-	
+	public static void main(String[] args) {
+		
+		Cryptography cryptography = new Cryptography();
+		
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print("Please enter first number to encrypt: ");
+		int fir = scanner.nextInt();
+		
+		System.out.print("Please enter second number to encrypt: ");
+		int sec = scanner.nextInt();
+		
+		System.out.print("Please enter third number to encrypt: ");
+		int thi = scanner.nextInt();
+		
+		System.out.print("Please enter fourth number to encrypt: ");
+		int fort = scanner.nextInt();
+		
+		cryptography.fourNumbersEncrypting(fir, sec, thi, fort);
+		
+	}
 	
 	
 }
