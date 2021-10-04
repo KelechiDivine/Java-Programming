c6bf391a6cd820fc3e010d8396078e80c1830ab4
Kelechi Divine <74909446+KelechiDivine@users.noreply.github.com>
2021-10-04 16:22:14 +0100

    finished sales commission question

--- src/main/java/chapterSeven/excercise/SalesCommissions.java
+++ src/main/java/chapterSeven/excercise/SalesCommissions.java
@@ -1,4 +1,22 @@
 package chapterSeven.excercise;
 
 public class SalesCommissions {
+	
+	private double salary;
+	private int range;
+	private final int[] total = new int[9];
+	
+	public void init(double dollar){
+		
+		while (dollar >= 0){
+			salary = dollar * 0.09 + 200;
+			range = (int) salary / 100;
+			
+			if (range > 10){
+				range = 10;
+			}
+			
+			++total[range - 2];
+		}
+	}
 }
