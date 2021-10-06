5a8af47ede87969baf5f0e48ffbb974272cc0572
Kelechi Divine <74909446+KelechiDivine@users.noreply.github.com>
2021-10-06 18:21:36 +0100

    odd and even number is done

--- src/main/java/hackerRank/OddAndEven.java
+++ src/main/java/hackerRank/OddAndEven.java
@@ -12,29 +12,23 @@ public class OddAndEven {
 	 * If n is even and in the inclusive range of 6 to 20, print Weird
 	 * If n is even and greater than 20, print Not Weird
 	 * Complete the stub code provided in your editor to print whether or not n is weird.**/
+
 	
-	
-	public static void main(String[] args){
-		Scanner sc=new Scanner(System.in);
-		int n=sc.nextInt();
+	public void init(int index){
+		if (index % 2 == 1)
+			System.out.println("Weird");
 		
-		if(n % 2 == 1){
+		if (index >= 2 && index <= 5)
+			System.out.println("Not weird!!");
+		
+		if (index >= 6 && index <= 20)
 			System.out.println("Weird");
-		}
-		else {
-			if (n >= 2 && n <= 5) {
-				System.out.println("Not Weird");
-			}
-			
-			else if (n >= 6 && n <= 20) {
-				System.out.println("Weird");
-			}
-			
-			else {
-				System.out.println("Not Weird");
-			}
-		}
-		if (n > 20)
-			System.out.println("Weird");
+		
+		else
+			System.out.println("Not Weird...");
+		
+		if (index > 20)
+			System.out.println("Weird...");
 	}
+	
 }
