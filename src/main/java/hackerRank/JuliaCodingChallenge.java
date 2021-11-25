e71c45d7a01dc509d9adbdff5f081d7d6b747de9
KelechiDivine <okoroaforkelechi123@gmail.com>
2021-11-25 16:54:49 +0100

    Can print user details

--- src/main/java/hackerRank/JuliaCodingChallenge.java
+++ src/main/java/hackerRank/JuliaCodingChallenge.java
@@ -2,4 +2,27 @@ package hackerRank;
 
 public class JuliaCodingChallenge {
 
+    private int hackerId;
+    private String hackerName;
+    private int challengeId;
+
+    public JuliaCodingChallenge(){}
+
+    public int getHackerId() {
+        return hackerId;
+    }
+
+    public String getHackerName() {
+        return hackerName;
+    }
+
+    public int getChallengeId() {
+        return challengeId;
+    }
+
+    public void displayHackersDetail() throws Exception {
+        System.out.println("Hacker's name       |       Hacker's id         | challenge id");
+        System.out.println("----------------------------------------------------------------------");
+        System.out.printf("%s                | %s                         | %s ", getHackerName(), getHackerId(), getChallengeId());
+    }
 }
--- src/test/java/hackerRank/JuliaCodingChallengeTest.java
+++ src/test/java/hackerRank/JuliaCodingChallengeTest.java
@@ -1,5 +1,29 @@
 package hackerRank;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+
 public class JuliaCodingChallengeTest {
 
+//    private JuliaCodingChallenge juliaCodingChallenge;
+//
+//    @BeforeEach
+//    public void setJuliaCodingChallenge(){
+//        juliaCodingChallenge = new JuliaCodingChallenge();
+//    }
+
+    @Test
+    public void assertJuliaCodingChallengeClassExist(){
+        JuliaCodingChallenge juliaCodingChallenge1 = new JuliaCodingChallenge();
+        Assertions.assertNotNull(juliaCodingChallenge1);
+    }
+
+    @Test
+    public void test_canDisplayHackersInformationInTableFormat() throws Exception {
+        JuliaCodingChallenge juliaCodingChallenge = new JuliaCodingChallenge();
+        juliaCodingChallenge.displayHackersDetail();
+        Assertions.assertTrue(true);
+    }
 }
