ea70d38b2ca5b1822738bcd846eafe665d06e016
KelechiDivine <okoroaforkelechi123@gmail.com>
2021-11-26 15:15:36 +0100

    Completed the counting valley

--- src/main/java/hackerRank/CountingValley.java
+++ src/main/java/hackerRank/CountingValley.java
@@ -2,5 +2,50 @@ package hackerRank;
 
 public class CountingValley {
 
+    /* The goal is to count the number of valleys a hiker goes though: */
 
+     /*                 PseudoCode
+
+     * -> Initialize the altitude to default (0)
+     * -> Initialize the valley to (0)
+     * -> Then,increment direction toward the hike step
+     * -> Check if the path is U.
+     * -> If the path is equals U, then keep incrementing the altitude
+     * -> If the altitude is still at default, then keep incrementing the valley
+     * -> Else if altitude is not equals U, the decrement the altitude
+     * -> return the valley.
+
+     */
+
+
+    //    TODO: Create counting valley method
+
+    public int countingValley(String direction, int step) {
+        int seaLevel = 0;
+        int valley = 0;
+
+        for (int stillInValley = 0; stillInValley < direction.length(); stillInValley++){
+            if (direction.charAt(stillInValley) == 'U'){
+                seaLevel ++;
+            }
+            else{
+                if (direction.charAt(stillInValley) == 'D'){
+                    if (seaLevel == 0){
+                        valley++;
+                    }
+                    seaLevel--;
+                }
+            }
+        }
+
+//        TODO: Print a descriptive information for the hiker.
+        System.out.printf("Number of steps: %d", step);
+        System.out.println();
+        System.out.printf("Number of direction: %s", direction);
+        System.out.println();
+        System.out.printf("Result: %d valley(s) ", valley);
+
+        return valley;
+
+    }
 }
--- src/test/java/hackerRank/CountingValleyTest.java
+++ src/test/java/hackerRank/CountingValleyTest.java
@@ -1,5 +1,63 @@
 package hackerRank;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class CountingValleyTest {
 
+    private CountingValley countingValley;
+
+    @BeforeEach
+    public  void setCountingValley(){
+        countingValley = new CountingValley();
+    }
+
+    @Test
+    public void assertCountingValleyClassExist(){
+        CountingValley countingValley = new CountingValley();
+        Assertions.assertNotNull(countingValley);
+    }
+
+    @Test
+    public void test_eightStepsValleyHike(){
+        int fromHike = countingValley.countingValley("UDDDUDUU", 8);
+        Assertions.assertEquals(1, fromHike, "NO hike value found.");
+    }
+
+    @Test
+    public void test_tenStepsValleyHike(){
+        int fromHike = countingValley.countingValley("UDDDUDUUDU", 10);
+        Assertions.assertEquals(2, fromHike, "No hike value found");
+    }
+
+    @Test
+    public void test_oneStepValleyHike(){
+        int fromHike = countingValley.countingValley("DU", 1);
+        Assertions.assertEquals(1, fromHike, "No hike value found.");
+    }
+
+    @Test
+    public void test_threeStepsValleyHike(){
+        int fromHike = countingValley.countingValley("DDU", 3);
+        Assertions.assertEquals(1, fromHike, "No hike value found.");
+    }
+
+    @Test
+    public void test_tenMillionValleyHike(){
+        int fromHike = countingValley.countingValley("DDU", 1000001);
+        Assertions.assertEquals(1, fromHike, "No hike value found.");
+    }
+
+    @Test
+    public void test_twentyValleyHike(){
+        int fromHike = countingValley.countingValley("DDUUDDUUDDUUDDUUDDUU", 20);
+        Assertions.assertEquals(5, fromHike, "No hike value found.");
+    }
+
+    @Test
+    public void test_tenNegativeValleyHike(){
+        int fromHike = countingValley.countingValley("UUUUUDUUUU", 10);
+        Assertions.assertEquals(0, fromHike, "No hike value found.");
+    }
 }
