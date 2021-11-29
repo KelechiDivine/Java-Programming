b8c93b045dee979e05a830cbb6b4c993caefa161
KelechiDivine <okoroaforkelechi123@gmail.com>
2021-11-29 10:58:49 +0100

    Refactored naming convention

--- src/main/java/hackerRank/StringCalculation.java
+++ src/main/java/hackerRank/StringCalculation.java
@@ -7,18 +7,18 @@ public class StringCalculation {
         int newNumber = 0;
         int value = 0;
 
-        for (int i = 0; i < number.length(); i++) {
-            if (number.charAt(i) == '0') value = 0;
-            if (number.charAt(i) == '1') value = 1;
-            if (number.charAt(i) == '2') value = 2;
-            if (number.charAt(i) == '3') value = 3;
-            if (number.charAt(i) == '4') value = 4;
-            if (number.charAt(i) == '5') value = 5;
-            if (number.charAt(i) == '6') value = 6;
-            if (number.charAt(i) == '7') value = 7;
-            if (number.charAt(i) == '8') value = 8;
-            if (number.charAt(i) == '9') value = 9;
-            newNumber += value * ((int)Math.pow(10, (number.length() - 1) - i));
+        for (int indexOf = 0; indexOf < number.length(); indexOf++) {
+            if (number.charAt(indexOf) == '0') value = 0;
+            if (number.charAt(indexOf) == '1') value = 1;
+            if (number.charAt(indexOf) == '2') value = 2;
+            if (number.charAt(indexOf) == '3') value = 3;
+            if (number.charAt(indexOf) == '4') value = 4;
+            if (number.charAt(indexOf) == '5') value = 5;
+            if (number.charAt(indexOf) == '6') value = 6;
+            if (number.charAt(indexOf) == '7') value = 7;
+            if (number.charAt(indexOf) == '8') value = 8;
+            if (number.charAt(indexOf) == '9') value = 9;
+            newNumber += value * ((int)Math.pow(10, (number.length() - 1) - indexOf));
         }
         return newNumber;
     }
