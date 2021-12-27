eaa29f9359e90fc458250db597d97a9d007bb1a6
KelechiDivine <okoroaforkelechi123@gmail.com>
2021-12-27 19:01:12 +0100

    created constructor

--- src/main/java/hackerRank/algo_expert/BST.java
+++ src/main/java/hackerRank/algo_expert/BST.java
@@ -2,4 +2,11 @@ package hackerRank.algo_expert;
 
 public class BST {
 
+    public int value;
+    public BST left;
+    public BST right;
+
+    public BST(int value) {
+        this.value = value;
+   }
 }
