3faa40239be08748ad05f62bcbc1d0ddfd29020f
KelechiDivine <okoroaforkelechi123@gmail.com>
2021-12-27 21:30:17 +0100

    implementing the closest number logic

--- src/main/java/hackerRank/SalesByMatch.java
+++ src/main/java/hackerRank/SalesByMatch.java
@@ -1,9 +1,6 @@
 package hackerRank;
 
-import chapterSeven.examples.Array;
-
 import java.util.Arrays;
-import java.util.List;
 
 public class SalesByMatch {
 
--- src/main/java/hackerRank/algo_expert/ValueInBst.java
+++ src/main/java/hackerRank/algo_expert/ValueInBst.java
@@ -1,73 +1,59 @@
 package hackerRank.algo_expert;
 
+import hackerRank.StringCalculation;
+
 public class ValueInBst {
     private BST bst;
+    private final StringCalculation stringCalculation = new StringCalculation();
 
     /* Write a function that takes in a tree and  an integer value and returns the closest value to that target value.
      */
 
-    public int findClosetValueInBST(BST tree, int target){
-        if (target == 0){
-            throw new NullPointerException("The target value can't be null.");
-        }
+    public int findClosetValueInBST(BST tree, int target) {
+        int closestBSTValue = 0;
 
-        if (target < 0){
-            throw new ArrayIndexOutOfBoundsException("Tree number can't be an integer.");
-        }
-
-        if (tree.left.value >= 0 ){
-            checkValueOnTheLeftBst();
-        }
-
-        if (tree.right.value >= 0){
-            checkValueOnTheRightBst();
-        }
-
-       return -1;
-    }
-
-//    TODO: Create a method that receives the assigned number of trees a user number wants.
-    public int value(int numberOfTree, int [] givenArray){
-//        TODO: Create a count variable and assign it to 0
-        int countTree = 0;
-
-//        TODO: Loop through the number of trees a user will enter
-        for (int treeNumberVariable = 0; treeNumberVariable < numberOfTree - 1 ; treeNumberVariable++){
-//            TODO: Check if the number of trees a user enter is equals to the number od trees assigned by a user
-            if (givenArray[treeNumberVariable] == givenArray[treeNumberVariable] - 1){
-                countTree++;
-                treeNumberVariable +=1;
+        for (int i = 0; i < tree.value; i++) {
+            closestBSTValue = tree.value;
+            if (closestBSTValue == target) {
+                System.out.println();
             }
         }
-//        TODO: Return the trees
+        return -1;
+    }
+
+    public int value(int numberOfTree, int[] givenArray) {
+        int countTree = 0;
+        for (int treeNumberVariable = 0; treeNumberVariable < numberOfTree - 1; treeNumberVariable++) {
+            if (givenArray[treeNumberVariable] == givenArray[treeNumberVariable] - 1) {
+                countTree++;
+                treeNumberVariable += 1;
+            }
+        }
         return countTree;
     }
 
-    public void checkValueOnTheRightBst(){
+    public void checkValueOnTheRightBst() {
         BST[] bstRightValue = new BST[1];
-
         bst.right = bstRightValue[0];
 
         if (bst.left.value == bst.right.value) {
             throw new IllegalArgumentException("The left node can't be equals to the right node.");
         }
 
-        if (bst.left.value> bst.right.value){
+        if (bst.left.value > bst.right.value) {
             throw new IllegalArgumentException("The left node can't be greater than the right node.");
         }
-}
+    }
 
-
-    public void checkValueOnTheLeftBst(){
+    public void checkValueOnTheLeftBst() {
         BST[] bstLeftValue = new BST[1];
-
         bst.left = bstLeftValue[0];
 
         if (bst.left.value == bst.right.value) {
             throw new IllegalArgumentException("The left node can't be equals to the right node.");
         }
 
-        if (bst.left.value> bst.right.value){
+        if (bst.left.value > bst.right.value) {
             throw new IllegalArgumentException("The left node can't be greater than the right node.");
         }
     }
