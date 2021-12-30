112b477c6b804d5a6f28621e5864e6c0c84ca968
KelechiDivine <okoroaforkelechi123@gmail.com>
2021-12-30 21:35:04 +0100

    completed employee test

--- src/main/java/chapterEight/example/EmployeeTest.java
+++ src/main/java/chapterEight/example/EmployeeTest.java
@@ -4,5 +4,10 @@ public class EmployeeTest {
 
     public static void main(String[] args) {
 
+        Date birth = new Date(7, 24, 1949);
+        Date hire = new Date(3, 12, 1988);
+        Employee employee = new Employee("Bob", "Blue", birth, hire);
+
+        System.out.println(employee);
     }
 }
