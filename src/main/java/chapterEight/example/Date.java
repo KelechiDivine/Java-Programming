e9a97f95a28a3720db3cbb192485964c76113f38
KelechiDivine <okoroaforkelechi123@gmail.com>
2021-12-30 21:18:49 +0100

    made date final

--- src/main/java/chapterEight/example/Date.java
+++ src/main/java/chapterEight/example/Date.java
@@ -2,9 +2,9 @@ package chapterEight.example;
 
 public class Date {
 
-    private int month;
-    private int day;
-    private int year;
+    private final int month;
+    private final int day;
+    private final int year;
 
     private static final int[] dayPerMonth = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
@@ -16,6 +16,19 @@ public class Date {
         if (day <= 0 || (day > dayPerMonth[month] && !(month == 2 && day == 29)))
             throw new IllegalArgumentException("day (" + day + ") out-of-range for the specified month and year");
 
-        if ()
+        if (month == 2 && day == 29 && !(year % 400 == 0 ||
+                (year % 4 == 0 && year % 100 != 0)))
+            throw new IllegalArgumentException("day (" + day + ") out-of-range for the specified month and year");
+
+        this.month = month;
+        this.day = day;
+        this.year = year;
+
+        System.out.printf("Date object constructor for date %s%n", this);
+    }
+
+    public String toString(){
+        return String.format("%d/%d/%d", month, day, year);
     }
 }
+
