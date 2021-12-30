d97ac368529a5f022b656d21810d7d6ba1c4a2f8
KelechiDivine <okoroaforkelechi123@gmail.com>
2021-12-30 21:52:33 +0100

    finished employee test

---
+++ src/main/java/chapterEight/example/Book.java
@@ -0,0 +1,27 @@
+package chapterEight.example;
+
+public enum Book {
+
+    JHTP("Java How to Program", "2015"),
+    CHTP("C How to Program", "2013"),
+    IW3HTP("Internet & World Wide Web How to Program", "2012"),
+    CPPHTP("C++ How to Program", "2014"),
+    VBHTP("Visual Basic How to Program", "2014"),
+    CSHARPHTP("Visual C# How to Program", "2014");
+
+    private final String title;
+    private final String copyrightYear;
+
+    Book(String title, String copyrightYear) {
+        this.title = title;
+        this.copyrightYear = copyrightYear;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getCopyrightYear() {
+        return copyrightYear;
+    }
+}
---
+++ src/main/java/chapterEight/example/BookTest.java
@@ -0,0 +1,18 @@
+package chapterEight.example;
+
+import java.util.EnumSet;
+
+public class BookTest {
+
+    public static void main(String[] args) {
+
+        for (Book book : Book.values())
+            System.out.printf("%-10s%-45s%s%n", book, book.getTitle(), book.getCopyrightYear());
+
+        System.out.printf("%nDisplay a range of enum constants:%n");
+
+        for (Book book : EnumSet.range(Book.JHTP, Book.CHTP))
+            System.out.printf("%-10s%-45s%s%n", book, book.getTitle(), book.getCopyrightYear());
+
+    }
+}
