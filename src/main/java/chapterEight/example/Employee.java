05c25a94a3bbfa19b993f887feb8bcfc690363f3
KelechiDivine <okoroaforkelechi123@gmail.com>
2021-12-30 21:22:14 +0100

    created employee class

---
+++ src/main/java/chapterEight/example/Employee.java
@@ -0,0 +1,16 @@
+package chapterEight.example;
+
+public class Employee {
+
+    private String firstName;
+    private String lastName;
+    private Date birthDate;
+    private Date hireDate;
+
+    public Employee(String firstName, String lastName, Date birthDate, Date hireDate) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthDate = birthDate;
+        this.hireDate = hireDate;
+    }
+}
