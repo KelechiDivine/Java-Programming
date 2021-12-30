f1d1fc2c770c1966cc6b76cd7f0b05010ad2c105
KelechiDivine <okoroaforkelechi123@gmail.com>
2021-12-30 23:30:21 +0100

    fixed interst  class

--- src/main/java/chapterEight/example/Interest.java
+++ src/main/java/chapterEight/example/Interest.java
@@ -10,14 +10,11 @@ public class Interest {
 		BigDecimal principal = BigDecimal.valueOf(1000.0);
 		BigDecimal rate = BigDecimal.valueOf(0.05);
 
-		System.out.printf(
-				"%s%20s%n", "Year",  "Amount on deposit"
-		);
+		System.out.printf("%s%20s%n", "Year",  "Amount on deposit");
 		
 		for (int year = 1; year <= 10; year++){
-			BigDecimal amount = principal.multiply(rate.add(
-					BigDecimal.ONE
-			).pow(year));
+			BigDecimal amount = principal.multiply(rate.add(BigDecimal.ONE).
+					pow(year));
 			
 			System.out.printf("%4d%20s%n", year, NumberFormat.getCurrencyInstance()
 			.format(amount));
