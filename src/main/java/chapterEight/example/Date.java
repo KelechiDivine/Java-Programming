7a595eb63747aae4bb96395b810795aecf8e4ac5
KelechiDivine <okoroaforkelechi123@gmail.com>
2021-12-30 21:09:48 +0100

    refactoring

---
+++ src/main/java/chapterEight/example/Date.java
@@ -0,0 +1,21 @@
+package chapterEight.example;
+
+public class Date {
+
+    private int month;
+    private int day;
+    private int year;
+
+    private static final int[] dayPerMonth = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+    public Date(int month, int day, int year){
+
+        if (month <= 0 || month > 12)
+            throw new IllegalArgumentException("month (" + month + ") must be 1-12");
+
+        if (day <= 0 || (day > dayPerMonth[month] && !(month == 2 && day == 29)))
+            throw new IllegalArgumentException("day (" + day + ") out-of-range for the specified month and year");
+
+        if ()
+    }
+}
