6de7e77fc5a023f62bf3a5c0ef38cc3bf5f7d318
KelechiDivine <okoroaforkelechi123@gmail.com>
2021-12-30 23:24:54 +0100

    fixed package data test

--- src/main/java/chapterEight/example/PackageDataTest.java
+++ src/main/java/chapterEight/example/PackageDataTest.java
@@ -1,4 +1,29 @@
 package chapterEight.example;
 
 public class PackageDataTest {
+
+    public static void main(String[] args) {
+
+        PackageData packageData = new PackageData();
+        System.out.printf("After instantiation: %n%s%n", packageData);
+
+        packageData.number = 77;
+        packageData.string = "Goodbye";
+
+        System.out.printf("%nAfter changing values: %n%s%n", packageData);
+    }
+}
+
+class PackageData {
+    int number;
+    String string;
+
+    public PackageData() {
+        number = 0;
+        string = "Hello";
+    }
+
+    public String toString() {
+        return String.format("number: %d: string %s", number, string);
+    }
 }
