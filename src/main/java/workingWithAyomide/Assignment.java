421dd16fc98eef8d48a4022f04b0b3eccf580abb
ZipDemon <okoroaforkelechi123@gmail.com>
2022-01-20 17:26:06 +0100

    Assignment operator

---
+++ src/main/java/workingWithAyomide/Assignment.java
@@ -0,0 +1,15 @@
+package workingWithAyomide;
+
+public class Assignment {
+
+    public static void main(String[] args) {
+        int num;
+        String name;
+
+        num = 10;
+        name = "GeeksForGeeks";
+
+        System.out.println("Num is assigned: " + num);
+        System.out.println("Name is assigned : " + name);
+    }
+}
