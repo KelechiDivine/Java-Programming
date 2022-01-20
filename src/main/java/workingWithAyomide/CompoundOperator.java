0db35b250cadf27dade7b1e3a7211f159d6c24e0
ZipDemon <okoroaforkelechi123@gmail.com>
2022-01-20 17:42:56 +0100

    Compound Operator

---
+++ src/main/java/workingWithAyomide/CompoundOperator.java
@@ -0,0 +1,20 @@
+package workingWithAyomide;
+
+public class CompoundOperator {
+
+    public static void main(String[] args) {
+
+        int numb = 1;
+        int numb2 = 2;
+
+        numb = numb + numb2;
+        System.out.println(numb);
+
+        int a = 8;
+        int sum1 = 10;
+
+        a = a + sum1;
+
+        System.out.println(a);
+    }
+}
