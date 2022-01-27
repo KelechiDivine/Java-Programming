8c4897d2d2ce9cb8cf0d2e663034a7c823521fda
ZipDemon <okoroaforkelechi123@gmail.com>
2022-01-27 12:12:15 +0100

    fixed @setter in java bean

--- src/test/java/cleanCode/effectiveJava/JavaBeanTest.java
+++ src/test/java/cleanCode/effectiveJava/JavaBeanTest.java
@@ -1,7 +1,21 @@
 package cleanCode.effectiveJava;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 public class JavaBeanTest {
+
+    @Test
+    public void test_createJavaBean(){
+        JavaBean javaBean = new JavaBean();
+        javaBean.setServingSize(240);
+        javaBean.setServings(8);
+        javaBean.setCalories(100);
+        javaBean.setSodium(35);
+        javaBean.setCarbohydrate(27);
+
+        Assertions.assertNotNull(javaBean);
+        System.out.println(javaBean);
+    }
 
 }
